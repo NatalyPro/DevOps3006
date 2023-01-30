766b9d11460d5d22a3c9e3b4a38b0a3812732939
NatalyPro <natalyperkas@gmail.com>
2023-01-30 13:54:19 +0200

    Test1 (#3)
    * add test
    
    * add test
    
    * add test
    
    * add test

--- 5.py
+++ 5.py
@@ -1,3 +1,8 @@
+import datetime
+import requests
+#(terminal: pip install requests)
+
+
 is_true = False
 a = 2
 b = 2.5
@@ -14,6 +19,10 @@ else:
     print("something")
 
 names = ["chanan", "tom", "zack"]
+print(type(names))
+
+if "tom" in names:
+    print()
 other_list = []
 name_to_find = "ariel"
 if name_to_find in names:
@@ -21,6 +30,10 @@ if name_to_find in names:
 
 if len(other_list) > 0:
     print("other list has values")
+
+numbers = range(2, 10)
+for a in numbers:
+    print(a)
 
 k = 5
 f = 5
@@ -33,5 +46,79 @@ elif type(k) is str :
 print(k is f)
 print(t == e)
 print(t is e)
+
+x = '-1'
+while x != 0:
+    if x != '-1':
+        print("failed")
+    x = input("please  enter a number")
+
+x = '-1'
+while x != 0:
+    x = x + 1
+    if x % 2 != 0:
+        continue
+    print("failed")
+    x = input("please  enter a number")
+
+
+if 0 == 0:
+    pass
+print("")
+
+
+numbers = range(100)
+for number in numbers:
+    if number % 7 == 0 or '7' in str(number):
+        print("boom!")
+    elif "7" in str(number):
+        print("SUPER")
+    else:
+        print(number)
+
+a = 3
+print(type(str(a)))
+
+
+def print_hi():
+    print('hi')
+    print('hi again')
+
+
+for i in range(50):
+    print_hi()
+
+print(datetime.datetime.now().minute)
+
+response = requests.get("https://github.com")
+print(response.status_code)
+
+
+file = open('read_this.txt', 'r')
+lines = file.readline()
+for line in lines:
+    print(line,end=' ')
+    if line == lines["chanan", "tom", "zack"]:
+        print("boom!")
+file.write('\ndriving')
+
+def write_name(name):
+    file = open('names.txt', 'a')
+    file.write('\n' + name)
+    file.close()
+
+def read_names():
+    file = open('names.txt')
+    for line in file.readlines():
+        print(f'Welcome {line}')
+
+write_name('doron')
+write_name('dan')
+write_name('moshe')
+read_names()
+
+
+
+
 
 
--- Assigment_2 .py
+++ Assigment_2 .py
@@ -1,4 +1,4 @@
-# A
+# 1
 x = 2
 y = 1
 if x > y:
@@ -6,12 +6,12 @@ if x > y:
 elif x < y:
     print("small")
 
-# B
+# 2
 for x in range(4):
     print(x)
 
 
-# C
+# 3
 def check_variable(season):
     if season == 1:
         return f"{season} = summer"
@@ -28,27 +28,24 @@ def check_variable(season):
 for x in range(1, 5):
     print(check_variable(x))
 
-# D
+
+# 4
 # will run 10 times , last value :10
-count = 1
-while count < 11:
-    print(count)
-    count = count + 1
 
 
-# E
+# 5
 def get_variables(age, last_name, current_currency, flew_abroad, apt_number):
     return f"{current_currency, age}, {last_name}, {current_currency}, {flew_abroad}, {apt_number}"
 
 
 print(get_variables(32, "P", "$", True, 4))
 
-# F
+# 6
 phone_number = input("enter your phone: ")
 print(f"phone number {phone_number}")
 
 
-# G
+# 7
 def print_hello():
     print("hello")
 
@@ -60,7 +57,7 @@ def calculate(number1, number2):
 calculate(5, 3.2)
 
 
-# H
+# 8
 def print_name(name):
     print(f"{name}")
 
@@ -73,15 +70,13 @@ def divide_number(number):
 divide_number(4)
 
 
-# I
+# 9
 def add_number(t, p):
-    result = t + p
-    return result
+    return t + p
 
 
 def receive_string(first, second):
-    result = first + " " + second
-    return result
+    return first + " " + second
 
 
 print(add_number(1, 2))
@@ -89,7 +84,7 @@ print(add_number(1, 2))
 print(receive_string("N", "P"))
 
 
-# K
+# 10
 def pyramid():
     for i in range(5):
         for j in range(i + 1):
@@ -98,3 +93,62 @@ def pyramid():
 
 
 pyramid()
+
+
+# 11
+def xshape():
+    for i in range(0, 7):
+        for j in range(0, 7):
+            if i == j or j == 7 - 1 - i:
+                print('*', end='')
+            else:
+                print(' ', end='')
+        print()
+
+
+xshape()
+
+
+# 12 (1)
+def program():
+    bla = 0
+    num = select_num()
+    for p in range(length(num)):
+        number = int(str(num)[p])
+        c = number + bla
+        bla = c
+        print(f"computed number {bla}")
+
+
+def select_num():
+    num = int(input("Enter ANY integer: "))
+    print(f"selected number {num}")
+    return num
+
+
+def length(num):
+    return len(str(num))
+
+
+program()
+
+
+# 12 (2)
+def select_num_2():
+    num = int(input("Enter an integer 11-99: "))
+    print(f"selected number {num}")
+    return num
+
+
+def program_2():
+    num = select_num_2()
+    if num < 10 or num > 99:
+        print(f"error. number {num} not supported")
+    else:
+        first = int(str(num)[0])
+        second = int(str(num)[- 1])
+        print(f"computed number {first + second}")
+        return first + second
+
+
+program_2()
